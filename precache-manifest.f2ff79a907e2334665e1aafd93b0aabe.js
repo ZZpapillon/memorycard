e4d7cc7c31b8624330e4e7bc75ac0aae7d2741fd
ZZPapillon <ipapillondeveloper@gmail.com>
2023-06-18 20:47:26 +0200

    Updates

---
+++ precache-manifest.f2ff79a907e2334665e1aafd93b0aabe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "30fd53e6a44e25cdf80d",
+    "url": "/memorycard/static/css/main.edc93514.chunk.css"
+  },
+  {
+    "revision": "30fd53e6a44e25cdf80d",
+    "url": "/memorycard/static/js/main.30fd53e6.chunk.js"
+  },
+  {
+    "revision": "a85fb299981e48d2a797",
+    "url": "/memorycard/static/js/1.a85fb299.chunk.js"
+  },
+  {
+    "revision": "784abd85591afbf951cd",
+    "url": "/memorycard/static/js/runtime~main.784abd85.js"
+  },
+  {
+    "revision": "eeb07187f91fb2635cc7e8b133a48d94",
+    "url": "/memorycard/static/media/poke.eeb07187.gif"
+  },
+  {
+    "revision": "9e3923825ba4a4fa967858f980b8460f",
+    "url": "/memorycard/static/media/logoP.9e392382.png"
+  },
+  {
+    "revision": "3f26bc9c866e180b778e63783c8e9ff7",
+    "url": "/memorycard/static/media/ball.3f26bc9c.png"
+  },
+  {
+    "revision": "c506fbe47de0275d688a46d08caa5648",
+    "url": "/memorycard/static/media/bg1.c506fbe4.jpeg"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/memorycard/static/media/Pokemon Solid.36d26dbc.ttf"
+  },
+  {
+    "revision": "a680702950842236476859aa93d112bf",
+    "url": "/memorycard/index.html"
+  }
+];
